91906d7edce2406a210e9be1f9a64ade24287653
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-10-31 17:30:37 +0900

    facebook:contents click event

--- projects/facebook_clone/main.js
+++ projects/facebook_clone/main.js
@@ -1,6 +1,32 @@
 const bellBtn = document.querySelector('.bell');
 const notice =  document.querySelector('.notice');
 
+const contents = document.querySelector('#contents');
+
 bellBtn.addEventListener('click',()=>{
     notice.classList.toggle('visible');
+});
+
+contents.addEventListener('click',(e)=>{
+
+    let target = e.target;
+
+    while(!target.getAttribute('data-name')){
+        target = target.parentNode;
+
+        if(target.nodeName === 'BODY'){
+            target = null;
+            return;
+        }
+    }
+
+    // console.log(target);
+
+    if(target.matches('[data-name="like"]')){
+        console.log('like-btn');
+        
+    }else if(target.matches('[data-name="more"]')){
+        console.log('more-btn');
+        target.classList.toggle('active');
+    }
 });
